c03b221bba002cae90b77618e563aaefa41a3b99
Abrar Dhalwala <adhalwala@aecordigital.com>
2021-01-12 19:05:52 +0530

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TButton />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TButton from 'vue-lib-test'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TButton
   }
 }
 </script>
